c8eb8bdba22e5ae9dd6d777048fc621e8b3c4e28
Ronak <75837235+iamronakgupta@users.noreply.github.com>
2022-11-21 19:57:46 +0530

    Updated

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,10 +46,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'key' => env('AKIAWPII2QIOGZ4ZHDQK'),
+            'secret' => env('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'),
+            'region' => env('ap-south-1'),
+            'bucket' => env('launderup'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
